f2e849bd579a677546ab172f3cefd76605c28812
Thitaseth Turongpun <bestthi2209@gmail.com>
2019-10-05 03:44:55 +0700

    change directory

--- mysite/settings.py
+++ mysite/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'mysite.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['e:\\project\\templates'],
+        'DIRS': ['e:\\creator\\templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
